8f372a4ef6120c2f24341959199c80f173aeb336
Todd McCabe <toddbenjaminmccabe@gmail.com>
2016-06-27 17:05:31 -0700

    Updated Gruntfile.js to copy raw css files on build

--- angular/Gruntfile.js
+++ angular/Gruntfile.js
@@ -429,7 +429,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'styles/*.css'
           ]
         }, {
           expand: true,
